8d2efe79e8f470c862203454f5f039b18faefd4d
spootmu <spoot@qq.com>
2015-08-12 15:27:04 +0800

    Initial Commit

---
+++ 新浪微博/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  新浪微博
+//
+//  Created by apple on 15/8/12.
+//  Copyright (c) 2015年 spoot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 新浪微博/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  新浪微博
+//
+//  Created by apple on 15/8/12.
+//  Copyright (c) 2015年 spoot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
